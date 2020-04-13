a9478c5e7d41bbc333b2ca43806e590e514e0406
riyandharne-123 <60026968+riyandharne-123@users.noreply.github.com>
2020-04-13 14:31:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+//For Live Projects
+window.addEventListener('load',function(){
+    document.querySelector('body').classList.add("loaded")  
+  });
+  //fade in elements with jquery
+  $(document).ready(function(){
+    $("#home").fadeIn(2500);
+
+});
